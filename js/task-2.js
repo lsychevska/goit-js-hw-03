0d3e076ffeabdfb11c302c4dc2eab61ecce3104d
LSychevska <liudmyla.sychevska@gmail.com>
2026-06-03 14:58:21 +0300

    hw3

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  let commonArray = firstArray.concat(secondArray);
+  let newArray = [];
+  for (let i = 0; i < maxLength; i++) {
+    newArray.push(commonArray[i]);
+  }
+
+  return newArray;
+}
+
+//results
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
